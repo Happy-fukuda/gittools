adb004af3eddf854d328c07e5dd9902c0dd297f8
Happy-fukuda <09049422117@docomo.ne.jp>
2021-04-02 14:34:05 +0900

    add push

--- git_tools.py
+++ git_tools.py
@@ -15,16 +15,16 @@ current_directory=os.path.expanduser("~/catkin_ws/src")
 dirs_list = [f for f in os.listdir(current_directory) if os.path.isdir(os.path.join(current_directory, f))]
 
 class GitTools():
-
     def gitpull(self,pkg_ls=[]):
-
+        if len(pkg_ls)==0 or ("all" not in pkg_ls and  set(pkg_ls)-set(dirs_list)):
+            print("not find pkg")
+            return 0
         try:
             for dir in dirs_list:
-                if len(pkg_ls)==0 or dir in pkg_ls or "all" in pkg_ls:
+                if dir in pkg_ls or "all" in pkg_ls:
                     pass
                 else:
                     continue
-
                 dir_path=current_directory+"/"+dir
                 os.chdir(dir_path)
                 master_branch=sup.run(["git remote show origin | grep 'HEAD branch' | awk '{print $NF}'"],
@@ -39,8 +39,46 @@ class GitTools():
                     repo.git.branch(branch_name)
                 repo.git.checkout(branch_name)
                 repo.git.pull()
-        except:
-            print("pull failed")
+        except Exception as e:
+            print(e)
+
+
+    def gitpush(self,pkg_ls=[]):
+        if len(pkg_ls)==0 or ("all" not in pkg_ls and  set(pkg_ls)-set(dirs_list)):
+            print("not find pkg")
+            return 0
+        try:
+            for dir in dirs_list:
+                if len(pkg_ls)==0 or dir in pkg_ls or "all" in pkg_ls:
+                    print("git push:"+dir)
+                    pass
+                else:
+                    continue
+                dir_path=current_directory+"/"+dir
+                os.chdir(dir_path)
+                repo=git.Repo()
+                print(repo.git.branch())
+                branch_name=input("which branch or make branch:")
+                if branch_name not in repo.git.branch():
+                    repo.git.branch(branch_name)
+                repo.git.checkout(branch_name)
+                commit_name="'"+input("commit name:")+"'"
+                repo.git.add('.')
+                print("add all")
+                repo.git.commit('-m',commit_name)
+                print("commit")
+                try:
+                    repo.remotes.origin.push('HEAD')
+                    print("finish push")
+                except Exception as e:
+                    print(e)
+                    repo.git.reset('--soft','HEAD~')
+                    print("reset --soft")
+
+
+        except Exception as e:
+            print(e)
+
 
 
 
@@ -49,6 +87,9 @@ def main():
     git_tool=GitTools()
     if args.pull:
         git_tool.gitpull(pkg_ls=args.pull)
+    if args.push:
+        git_tool.gitpush(pkg_ls=args.push)
+
 if __name__=='__main__':
     print("git tools")
     main()
--- main_pro.py
+++ main_pro.py
@@ -12,42 +12,42 @@ def get_args():
     parser = argparse.ArgumentParser()
     #parser.add_argument("pkg",type=str,help="all or you want to run pkg"
     parser.add_argument("-p","--pull", help="Run 'git pull'. --pull {all or want pkg}",nargs='*')
-    parser.add_argument("-l","--log", help="You can see log. if you use it,you must before other option",action="store_true")
-    ''''
-    parser.add_argument("",)
-    parser.add_argument("--alert", help="optional", action="store_true")
-    '''
+    parser.add_argument("-s","--push", help="Run 'git push'. --push {all or want pkg}",nargs='*')
     # 結果を受ける
     args = parser.parse_args()
 
     return(args)
 
-def rungit(cmd,log):
+def rungit(cmd):
     git_username=input("git username:")
     git_password=getpass("git password:")
     user_pattern="Username .*"
     pass_pattern="Password .*"
-    error_pattern="pull failed"
-    expect_list=[user_pattern,pass_pattern,pex.EOF,error_pattern]
-    if log:
-        git_cmd = pex.spawn(cmd,logfile=sys.stdout.buffer)
-    else:
-        git_cmd = pex.spawn(cmd)
+    branch_select="which branch or make branch:"
+    commit_name="commit name:"
+    expect_list=[user_pattern,pass_pattern,branch_select,commit_name,
+                pex.EOF]
+    git_cmd = pex.spawn(cmd)
+    git_cmd.logfile_read=sys.stdout.buffer
+
     if git_cmd.isalive():
         while 1:
             e_num=git_cmd.expect(expect_list)
             if e_num==0:
                 git_cmd.sendline(git_username)
                 #print(expect_list[e_num]+"******入力しました")
-            if e_num==1:
+            elif e_num==1:
                 git_cmd.sendline(git_password)
                 #print(expect_list[e_num]+"******入力しました")
-            if e_num==2:
-                print("pull完了")
-                break
+            elif e_num==2:
+                branch_name=input()
+                git_cmd.sendline(branch_name)
             if e_num==3:
-                print("pull失敗,usernameとpasswordを入力し直してみてください")
+                commit=input()
+                git_cmd.sendline(commit)
+            if e_num==len(expect_list)-1:
                 break
+
     else:
         print("not alive")
     git_cmd.close()
@@ -58,8 +58,11 @@ def main():
     args = get_args()
     if args.pull:
         git_arg="python3 "+current_directory +"/git_tools.py " + "--pull " + ' '.join(args.pull)
-        rungit(git_arg,args.log)
-
+        print(git_arg)
+        rungit(git_arg)
+    if args.push:
+        git_arg="python3 "+current_directory +"/git_tools.py " + "--push " + ' '.join(args.push)
+        rungit(git_arg)
 
 if __name__ == '__main__':
     main()
